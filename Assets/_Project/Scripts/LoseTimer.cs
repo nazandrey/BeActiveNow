99c47bc2b5975f0f97160e4f86c9649022f06c52
nazandrey <nazandrbox@yandex.ru>
2020-10-12 17:49:14 +0300

    add player and lose timer which restart level if player doesn't move

---
+++ Assets/_Project/Scripts/LoseTimer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class LoseTimer : MonoBehaviour
+    {
+        public float loseTimeout;
+        public Moveable player;
+
+        private Coroutine _timer;
+
+        private void Start()
+        {
+            StartTimer();
+        }
+
+        private void Update()
+        {
+            if (player.IsMoving)
+                ResetTimer();
+        }
+
+        private void OnDestroy()
+        {
+            if (_timer != null)
+            {
+                DestroyCurrTimer();
+            }
+        }
+
+        private void StartTimer()
+        {
+            _timer = StartCoroutine(TimerCoroutine());
+        }
+
+        private IEnumerator TimerCoroutine()
+        {
+            yield return new WaitForSeconds(loseTimeout);
+            print("Lose");
+            SceneLoader.Instance.LoadFirstLevel();
+        }
+
+        private void ResetTimer()
+        {
+            DestroyCurrTimer();
+            StartTimer();
+        }
+
+        private void DestroyCurrTimer()
+        {
+            StopCoroutine(_timer);
+            _timer = null;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Moveable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Moveable : MonoBehaviour
+    {
+        public float speed;
+        public float moveThreshold;
+
+        public bool IsMoving { get; private set; }
+
+        private void FixedUpdate()
+        {
+            var translation = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+            
+            if (Math.Abs(translation) > moveThreshold)
+            {
+                IsMoving = true;
+                transform.Translate(translation, 0, 0);
+            }
+            else
+            {
+                IsMoving = false;
+            }
+        }
+    }
+}
