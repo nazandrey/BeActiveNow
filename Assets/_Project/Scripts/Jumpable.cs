200e5774b96d598d7d38ff839e8d40fc23e120fc
nazandrey <nazandrbox@yandex.ru>
2020-10-12 23:04:45 +0300

    add jump for player

---
+++ Assets/_Project/Scripts/Jumpable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+namespace _Project.Scripts
+{
+    [RequireComponent(typeof(Rigidbody2D))] 
+    public class Jumpable : MonoBehaviour
+    {
+        public float jumpSpeed;
+        
+        private Rigidbody2D _rigidbody;
+        private bool _isGrounded;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            //mb will work bad with touching other platforms from aside
+            if (other.gameObject.layer == LayerMask.NameToLayer("Floor"))
+                _isGrounded = true;
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _isGrounded = false;
+                _rigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
