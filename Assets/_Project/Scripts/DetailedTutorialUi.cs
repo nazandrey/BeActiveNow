91e42c35f394d4b6e43cfc24f9818c232ffd8edc
nazandrey <nazandrbox@yandex.ru>
2020-10-14 23:41:09 +0300

    add tutorial for level 0; hide pause button if detailed tutorial is shown (bugfix)

---
+++ Assets/_Project/Scripts/DetailedTutorialUi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts
+{
+    public class DetailedTutorialUi : MonoBehaviour
+    {
+        public PauseSetter pauseSetter;
+        public GameObject root;
+        public Text text;
+
+        private void Awake()
+        {
+            PlayerLevel.Instance.LevelUp += OnLevelUp;
+        }
+
+        private void Start()
+        {
+            root.SetActive(false);
+            if (PlayerLevel.Instance.CurrLevel == 0)
+                ShowTutorialText(0);
+        }
+
+        private void OnDestroy()
+        {
+            PlayerLevel.Instance.LevelUp -= OnLevelUp;
+        }
+
+        private void OnLevelUp(int currLevel)
+        {
+            if (currLevel > 1)
+                return;
+
+            ShowTutorialText(currLevel);
+        }
+
+        private void ShowTutorialText(int currLevel)
+        {
+            if (currLevel == 0)
+            {
+                text.text =
+                    "Do action above to avoid deep thinking and to seize the day";
+            }
+            else if (currLevel == 1)
+            {
+                text.text =
+                    "Now you need to do ALL actions above (it has reset timer) to seize the day";
+            }
+
+            text.text += "\n\nNod if you understand";
+            pauseSetter.Pause();
+            pauseSetter.gameObject.SetActive(false);
+            root.SetActive(true);
+        }
+
+        public void OnTutorialCompleted()
+        {
+            pauseSetter.gameObject.SetActive(true);
+            pauseSetter.Unpause();
+            root.SetActive(false);
+        }
+    }
+}
