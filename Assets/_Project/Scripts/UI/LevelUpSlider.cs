c1ae3f7c794e1712bdf3b26764b25eea0837b29f
nazandrey <nazandrbox@yandex.ru>
2020-10-16 00:29:15 +0300

    add colors to level up slider

--- Assets/_Project/Scripts/UI/LevelUpSlider.cs
+++ Assets/_Project/Scripts/UI/LevelUpSlider.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 
 namespace _Project.Scripts
 {
-    [RequireComponent(typeof(Slider))]
     public class LevelUpSlider : MonoBehaviour
     {
-        private Slider _slider;
+        public Color fromColor;
+        public Color toColor;
+        public Image fillImage;
+        public RectTransform handle;
+        
         private float _progressTimeout;
         private float _currProgress;
         
@@ -16,18 +19,17 @@ namespace _Project.Scripts
             _currProgress = 0;
             _progressTimeout = progressTimeout;
         }
-
-        private void Awake()
-        {
-            _slider = GetComponent<Slider>();
-        }
         
         private void Update()
         {
             if (_currProgress < _progressTimeout)
             {
                 _currProgress += Time.deltaTime;
-                _slider.value = _currProgress / _progressTimeout;
+                var progressNormalized = _currProgress / _progressTimeout;
+                fillImage.fillAmount = progressNormalized;
+                fillImage.color = Color.LerpUnclamped(fromColor, toColor, progressNormalized);
+                handle.anchorMin = new Vector2(progressNormalized, handle.anchorMin.y);
+                handle.anchorMax = new Vector2(progressNormalized, handle.anchorMax.y);
             }
         }
     }
